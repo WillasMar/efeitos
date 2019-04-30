834c94d6dfa339b7b18a061d3fd16a6b8ad769d4
Willas Mar <willasanime@yahoo.com.br>
2019-04-30 11:31:33 -0300

    Campos input, efeito na descrição do campo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
 $(function(){
+	var ativo = '';
 	
+	$('.campo').bind('click', function(){
+		ativo = $(this).attr('data-ativo');
+
+		if(ativo == 'off'){
+			$(this).find('label').addClass('label');
+			$(this).attr('data-ativo','on');
+			$(this).find('input').focus();
+			$(this).css('box-shadow','0px 0px 3px 0px blue');
+		}
+	});
+
+	$('input').bind('blur', function(){
+		$(this).parent().find('label').removeClass('label');
+		$(this).parent().attr('data-ativo','off');
+		$(this).parent().css('box-shadow','0px 0px 0px 0px blue');
+	});
 });
